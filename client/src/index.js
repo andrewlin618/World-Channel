ccc6085e556b8b545348f07ccf79bd81cadf592e
Andrew Lin <andrewlin618@gmail.com>
2020-09-23 18:52:45 -0700

    test server

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <h1>Test</h1>
   </React.StrictMode>,
   document.getElementById('root')
 );
--- server.js
+++ server.js
@@ -2,6 +2,14 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
+const http = require("http");
+const server = http.createServer(app);
+
+const socket = require("socket.io");
+const io = socket(server);
+
+var onlineUsers = {};
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -11,15 +19,51 @@ if (process.env.NODE_ENV === "production") {
 }
 
 app.get('/api', (req, res) => {
-    res.send(req.body);
+    res.send({ "hi": "hello" });
 })
 
 app.use("*", (req, res) =>
     res.sendFile(path.join(__dirname, "/client/build/index.html"))
 );
 
+// io.on("connection", SocketManager);
+
+io.on("connection", socket => {
+    socket.on("join", user => {
+        const newUser = {
+            name: user.username,
+            id: socket.id,
+            avatar: user.avatar,
+        }
+        socket.name = newUser.name;
+        socket.avatar = newUser.avatar;
+        onlineUsers[socket.id] = newUser;
+        io.emit("online users", Object.values(onlineUsers));
+        console.log(`=> ${socket.name} joined the room...`);
+        socket.emit("join", newUser)
+        socket.broadcast.emit("notification", `${newUser.name} joined the room...`);
+        // io.emit("notification", `${connectionCounter} ${connectionCounter < 2 ? 'user' : 'users'} online`);
+    })
+
+    socket.on("send message", messageObject => {
+        console.log(messageObject);
+        socket.broadcast.emit("message", messageObject)
+    })
+
+    socket.on("disconnect", () => {
+        if (socket.name) {
+            delete onlineUsers[socket.id];
+            io.emit("online users", Object.values(onlineUsers));
+            socket.broadcast.emit("notification", `${socket.name} left the room...`);
+            // io.emit("notification", `${connectionCounter} ${connectionCounter < 2 ? 'user' : 'users'} online`);
+
+        }
+        console.log(`=> ${socket.name} left the room...`)
+    })
+})
+
 const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server is running on PORT ${PORT}...`);
 })
