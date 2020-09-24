735e07ec2c3fc26c32b71b3d8f79f3d646868980
Andrew Lin <andrewlin618@gmail.com>
2020-09-23 19:11:13 -0700

    test server

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-const http = require("http");
-const server = http.createServer(app);
+// const http = require("http");
+// const server = http.createServer(app);
 
-const socket = require("socket.io");
-const io = socket(server);
+// const socket = require("socket.io");
+// const io = socket(server);
 
-var onlineUsers = {};
+// var onlineUsers = {};
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -28,42 +28,42 @@ app.use("*", (req, res) =>
 
 // io.on("connection", SocketManager);
 
-io.on("connection", socket => {
-    socket.on("join", user => {
-        const newUser = {
-            name: user.username,
-            id: socket.id,
-            avatar: user.avatar,
-        }
-        socket.name = newUser.name;
-        socket.avatar = newUser.avatar;
-        onlineUsers[socket.id] = newUser;
-        io.emit("online users", Object.values(onlineUsers));
-        console.log(`=> ${socket.name} joined the room...`);
-        socket.emit("join", newUser)
-        socket.broadcast.emit("notification", `${newUser.name} joined the room...`);
-        // io.emit("notification", `${connectionCounter} ${connectionCounter < 2 ? 'user' : 'users'} online`);
-    })
+// io.on("connection", socket => {
+//     socket.on("join", user => {
+//         const newUser = {
+//             name: user.username,
+//             id: socket.id,
+//             avatar: user.avatar,
+//         }
+//         socket.name = newUser.name;
+//         socket.avatar = newUser.avatar;
+//         onlineUsers[socket.id] = newUser;
+//         io.emit("online users", Object.values(onlineUsers));
+//         console.log(`=> ${socket.name} joined the room...`);
+//         socket.emit("join", newUser)
+//         socket.broadcast.emit("notification", `${newUser.name} joined the room...`);
+//         // io.emit("notification", `${connectionCounter} ${connectionCounter < 2 ? 'user' : 'users'} online`);
+//     })
 
-    socket.on("send message", messageObject => {
-        console.log(messageObject);
-        socket.broadcast.emit("message", messageObject)
-    })
+//     socket.on("send message", messageObject => {
+//         console.log(messageObject);
+//         socket.broadcast.emit("message", messageObject)
+//     })
 
-    socket.on("disconnect", () => {
-        if (socket.name) {
-            delete onlineUsers[socket.id];
-            io.emit("online users", Object.values(onlineUsers));
-            socket.broadcast.emit("notification", `${socket.name} left the room...`);
-            // io.emit("notification", `${connectionCounter} ${connectionCounter < 2 ? 'user' : 'users'} online`);
+//     socket.on("disconnect", () => {
+//         if (socket.name) {
+//             delete onlineUsers[socket.id];
+//             io.emit("online users", Object.values(onlineUsers));
+//             socket.broadcast.emit("notification", `${socket.name} left the room...`);
+//             // io.emit("notification", `${connectionCounter} ${connectionCounter < 2 ? 'user' : 'users'} online`);
 
-        }
-        console.log(`=> ${socket.name} left the room...`)
-    })
-})
+//         }
+//         console.log(`=> ${socket.name} left the room...`)
+//     })
+// })
 
 const PORT = process.env.PORT || 3001;
 
-server.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log(`Server is running on PORT ${PORT}...`);
 })
